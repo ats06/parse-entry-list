285763c4c466492769a2fd9b545be168f7da0c60
a2c <jobalbel@gmail.com>
2024-12-25 13:27:22 +0900

    refactoring

--- ExtractNameWithBib.py
+++ ExtractNameWithBib.py
@@ -11,7 +11,6 @@ import sys
 
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
-from pathlib import Path
 
 args = sys.argv
 if len(args) != 3:
@@ -20,36 +19,39 @@ if len(args) != 3:
 
 member_list_fname = args[1]
 entry_list_fname = args[2]
-#member_list_fname = 'member_list.csv'  # Todo: Get file from args
 
-# Read PDF into buffer
-fp_out = StringIO()
-with open(Path(entry_list_fname), 'rb') as fp_in:
-    extract_text_to_fp(fp_in, fp_out)
+# Read PDF as plane text
+def extract_text_from_pdf(pdf_path):
+    fp_out = StringIO()
+    with open(pdf_path, 'rb') as fp_in:
+        extract_text_to_fp(fp_in, fp_out)
+        # Replace code from Newpage(0x0c) to Newline(0x0a)
+        # It seems that a page is extracted as consists of one line and Newpage character
+        text = fp_out.getvalue()
+        return text.replace(chr(12), chr(10))
 
-# Replace code from Newpage(0x0c) to Newline(0x0a)
-# It seems that a page is extracted as consists of one line and Newpage character
-text = fp_out.getvalue()
-text = text.replace(chr(12), chr(10))  
-#print(text)
+# Read member list as matrix
+# [['山田','太郎'],['トレイル','ランガ'], ...]
+def extract_member_from_list(list_path):
+    with open(list_path, encoding='utf8', newline='') as f:
+        reader = csv.reader(f)
+        return [row for row in reader]
+
+text = extract_text_from_pdf(entry_list_fname)
+members = extract_member_from_list(member_list_fname)
 
 print('===== Entries =====')
+for member in members:
+    # Find string "Family name + Zenkaku/Hankaku/Tab space + Last name".
+    # Family name: row[0], Last name: row[1]
+    pattern = member[0] + '[\\s\\u3000\\t]*' + member[1]
+    m = re.search(pattern, text)
 
-with open(member_list_fname, encoding='utf8', newline='') as f:
-    csvreader = csv.reader(f)
-    for row in csvreader:
-        # Find string "Family name + Zenkaku/Hankaku/Tab space + Last name".
-        # Family name: row[0], Last name: row[1]
-        pattern = row[0] + '[\\s\\u3000\\t]*' + row[1]
-        m = re.search(pattern, text)
-        if m != None:
-            
-            name = m.group().replace('\u3000', ' ')
-            # Assume that bib number is in front of name within eight character.
-            # Show the nearest one to name as bib number in case multiple figures found.
-            bib = re.findall(r'\d+', text[m.start()-4:m.start()])
-            if len(bib) != 0:
-                print(f'{bib[len(bib)-1]} {name}')
-            else:
-                print(f'{name}')
-            
+    if m != None:
+        name = m.group().replace('\u3000', ' ')
+        # Assume that bib number is in front of name within eight character.
+        # Show the nearest one to name as bib number in case multiple figures found.
+        bib = re.findall(r'\d+', text[m.start()-4:m.start()])
+        bib_number = bib[-1] if bib else ''
+
+        print('{:<5} {}'.format(bib_number, name))
